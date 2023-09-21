ac9c5f2d750d8e28e41d1c2bb2436a56ddd21dfc
Mayo <mayocream39@yahoo.co.jp>
2023-09-21 10:05:07 +0800

    add scale

--- tsboard/src/components/ImageWindow.tsx
+++ tsboard/src/components/ImageWindow.tsx
@@ -1,55 +1,85 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import SampleImage01 from '../assets/images/sample_01.jpg'
-import { fabric } from 'fabric'
+import * as fabric from 'fabric'
 
 function ImageWindow({ imageUrl = SampleImage01 }) {
-  const canvasParentRef = useRef<HTMLDivElement>(null)
+  const canvasWindowRef = useRef<HTMLDivElement>(null)
   const canvasNodeRef = useRef<HTMLCanvasElement>(null)
   const canvasRef = useRef<fabric.Canvas>()
+  const zoomInputRef = useRef<HTMLInputElement>(null)
+  const [currentZoom, setCurrentZoom] = useState(1)
 
-  useEffect(() => {
-    const canvas = new fabric.Canvas(canvasNodeRef.current, {
-      width: canvasParentRef.current?.clientWidth,
-      height: window.innerHeight * 2,
-    })
-    canvasRef.current = canvas
-    fabric.Image.fromURL(
-      imageUrl,
-      (img) => {
-        img.scaleToHeight(window.innerHeight)
-        canvas.add(img)
-        canvas.centerObject(img)
-      },
-      {
-        hasControls: false,
-      }
-    )
-    canvasParentRef.current?.scrollTo({
-      top: window.innerHeight / 2,
-    })
+  useLayoutEffect(() => {
+    initCanvas()
 
-    canvas.on('mouse:wheel', (opt) => {
-      if (!opt.e.ctrlKey) return
-
-      var delta = opt.e.deltaY
-      var zoom = canvas.getZoom()
-      zoom *= 0.999 ** delta
-      if (zoom > 20) zoom = 20
-      if (zoom < 0.01) zoom = 0.01
-      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom)
-      opt.e.preventDefault()
-      opt.e.stopPropagation()
-    })
+    return () => {
+      canvasRef.current?.dispose()
+    }
   }, [])
+
+  const initCanvas = async () => {
+    // canvas size equal to image size (retina display)
+    const img = await fabric.Image.fromURL(imageUrl, {}, {})
+    const canvas = new fabric.Canvas(canvasNodeRef.current!, {
+      backgroundImage: img,
+    })
+    canvas.setDimensions({ height: img.height, width: img.width }, { backstoreOnly: true })
+    canvas.renderAll()
+    canvasRef.current = canvas
+
+    // zoom canvas to fit screen
+    if (img.height > window.innerHeight) {
+      const zoom = window.innerHeight / img.height
+      canvas.setDimensions({ height: window.innerHeight, width: zoom * img.width }, { cssOnly: true })
+      displayZoomWithUnit(zoom)
+    }
+    if (img.width > canvasWindowRef.current!.clientWidth) {
+      const zoom = canvasWindowRef.current!.clientWidth / img.width
+      canvas.setDimensions({ height: zoom * img.height, width: canvasWindowRef.current!.clientWidth }, { cssOnly: true })
+      displayZoomWithUnit(zoom)
+    }
+  }
+
+  const displayZoomWithUnit = (zoom: number) => {
+    setCurrentZoom(zoom)
+    zoomInputRef.current!.value = `${(zoom * 100).toPrecision(4)}%`
+  }
+
+  const handleZoomChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const zoom = parseFloat(e.target.value) / 100
+
+    // FIXME: canvas width can't larger than window width
+    if (canvasRef.current!.width * zoom > canvasWindowRef.current!.clientWidth) {
+      zoomInputRef.current!.value = `${(currentZoom * 100).toPrecision(4)}%`
+      return
+    }
+
+    const canvas = canvasRef.current!
+    canvas.setDimensions({ height: canvas.height * zoom, width: canvas.width * zoom }, { cssOnly: true })
+    displayZoomWithUnit(zoom)
+  }
+
+  const handleZoomEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      zoomInputRef.current?.blur()
+    }
+  }
 
   return (
     <div className='flex flex-col h-screen'>
-      <div className='flex justify-center grow preview overflow-auto' ref={canvasParentRef}>
+      <div className='flex grow justify-center preview overflow-auto' ref={canvasWindowRef}>
         <canvas ref={canvasNodeRef} />
       </div>
-      {/* <div className='bg-gray-500'>
-        <input className='w-[75px] text-[14px] text-center' type='number' step={5} defaultValue={100} />
-      </div> */}
+      <div className='flex bg-base-200 h-[.8rem]'>
+        <input
+          ref={zoomInputRef}
+          onBlur={handleZoomChange}
+          onKeyDown={handleZoomEnter}
+          className='w-[75px] text-[12px] text-center focus:outline-none bg-base-content'
+          type='text'
+          onChange={(e) => (zoomInputRef.current!.value = e.target.value)}
+        />
+      </div>
     </div>
   )
 }
--- tsboard/tailwind.config.js
+++ tsboard/tailwind.config.js
@@ -10,5 +10,5 @@ export default {
   plugins: [require("daisyui")],
   daisyui: {
     themes: ['dark'],
-  }
+  },
 }
