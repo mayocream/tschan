55040569c8f7e2b2a61ffda062417c35490bb5db
Mayo <mayocream39@yahoo.co.jp>
2023-10-05 05:03:54 +0800

    imporve incubator

--- incubator/main.py
+++ incubator/main.py
@@ -3,8 +3,7 @@ from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from manga_ocr import MangaOcr
 from json import loads, dumps
 from PIL import Image
-import cgi
-import os
+import io
 import logging
 
 
@@ -19,14 +18,9 @@ logging.info("loaded!")
 
 
 def inference_manga_ocr_h(self):
-    content_type, pdict = cgi.parse_header(self.headers['content-type'])
-    pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
-    form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={
-                            'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], })
-    fileitem = form['image']
+    file = self.rfile.read(int(self.headers['Content-Length']))
 
-    image = Image.open(fileitem.file)
-    image.save(os.path.join("uploaded", fileitem.filename))
+    image = Image.open(io.BytesIO(file))
 
     logging.info("Inference manga ocr...")
 
@@ -47,8 +41,18 @@ class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
+
+    def do_OPTIONS(self):
+        logging.info("OPTIONS request, Path: %s", str(self.path))
+        self._set_response()
+
+    def do_HEAD(self):
+        logging.info("HEAD request, Path: %s", str(self.path))
+        self._set_response()
 
     def do_GET(self):
         logging.info("GET request, Path: %s", str(self.path))
