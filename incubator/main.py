8d0d1cd7ddfa65501436ce2fe2c1cf1c18ed12ab
Mayo <mayocream39@yahoo.co.jp>
2023-10-10 08:09:16 +0800

    switch to flask which may reduce  panic

--- incubator/main.py
+++ incubator/main.py
@@ -1,121 +1,70 @@
-#!/usr/bin/env python3
-from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import cv2
 import numpy as np
 from ctd import ComicTextDetector
 from manga_ocr import MangaOcr
-from json import loads, dumps
 from PIL import Image
-import cgi
-import logging
+from loguru import logger
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+app = Flask(__name__)
+CORS(app)
 
-logging.info("Preloading manga ocr model...")
+logger.info("Preloading manga ocr model...")
 mocr = MangaOcr()
-logging.info("loaded!")
+logger.info("loaded!")
 
-logging.info("Preloading comic text detector model...")
+logger.info("Preloading comic text detector model...")
 ctd = ComicTextDetector(manga_ocr_model=mocr)
-logging.info("loaded!")
+logger.info("loaded!")
 
 
-def inference_manga_ocr_h(self):
-    fileitem = self._get_image()
+@app.route("/magic/manga-ocr", methods=['POST'])
+def inference_manga_ocr():
+    if 'image' not in request.files:
+        return jsonify({"error": "Image not provided"}), 400
 
-    image = Image.open(fileitem.file)
+    fileitem = request.files['image']
+    image = Image.open(fileitem)
 
-    logging.info("Inference manga ocr...")
+    logger.info("Inference manga ocr...")
 
     # inference
     text = mocr(image)
 
-    logging.info("Inference done!, result: %s", text)
+    logger.info("Inference done!, result: {}", text)
 
-    return dumps(text)
+    return jsonify(text)
 
 
-# image resized to 1024x1024 before inference
-def inference_comic_text_detector(self):
-    fileitem = self._get_image()
+@app.route("/magic/comic-text-detector+manga-ocr", methods=['POST'])
+def inference_comic_text_detector():
+    if 'image' not in request.files:
+        return jsonify({"error": "Image not provided"}), 400
 
-    image = cv2.imdecode(np.fromfile(
-        fileitem.file, dtype=np.uint8), cv2.IMREAD_COLOR)
+    fileitem = request.files['image']
 
-    logging.info("Inference comic text detector & manga ocr...")
+    npimg = np.frombuffer(fileitem.read(), np.uint8)
+    image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    logger.info("Inference comic text detector & manga ocr...")
 
     # inference
     _, mask, blks = ctd(image)
 
-    logging.info("Inference done!, result: %s", blks)
+    logger.info("Inference done!, result: {}", blks)
 
-    return dumps({
+    return jsonify({
         # 'mask': mask.tolist(),
         'blks': blks
     })
 
 
-h = {
-    "/magic/manga-ocr": inference_manga_ocr_h,
-    "/magic/comic-text-detector+manga-ocr": inference_comic_text_detector,
-}
-
-
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers',
-                         'Origin, Content-Type, Accept')
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-    def _get_image(self):
-        content_type, pdict = cgi.parse_header(self.headers['content-type'])
-        pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
-        form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={
-                                'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], })
-        fileitem = form['image']
-        return fileitem
-
-    def do_OPTIONS(self):
-        self._set_response()
-
-    def do_GET(self):
-        self._set_response()
-        self.wfile.write("XOXO".encode('utf-8'))
-
-    def do_POST(self):
-
-        # handles the request
-        # error thrown in thread if not hit the handler
-        response = h[self.path](self)
-
-        self._set_response()
-        self.wfile.write(response.encode('utf-8'))
-
-
-def run(handler_class=S, port=43101):
-    server_address = ('', port)
-    httpd = ThreadingHTTPServer(server_address, handler_class)
-    logging.info('Starting httpd...\n')
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+@app.route("/", methods=['GET'])
+def index():
+    return "XOXO"
 
 
 if __name__ == '__main__':
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+    port = 43101
+    app.run(port=port)
